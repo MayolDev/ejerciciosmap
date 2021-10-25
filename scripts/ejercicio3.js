56f9312cd7271a3932888c626ba37bb0fda7437b
Iván Mayol Braza <imayoldev@gmail.com>
2021-10-25 09:12:06 +0200

    Metemos ejercicio 3 sin hacer

---
+++ scripts/ejercicio3.js
@@ -0,0 +1,12 @@
+
+
+const mapa = new Map([
+    ["Cádiz" , 29303],
+    ["Jerez de la Frontera", 12032],
+    ["Chiclana", 22392],
+    ["Madrid", 928392],
+    ["Barcelona" , 982388]
+    
+  ]);
+  
+
